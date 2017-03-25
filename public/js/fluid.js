629c52f0974f85564a7e8ade044c7eaf3ff726d9
James <jwin4740@gmail.com>
2017-03-25 11:06:41 -0400

    testmerge

--- public/js/fluid.js
+++ public/js/fluid.js
@@ -6,6 +6,32 @@
     <svg id="fillgauge6" width="19%" height="200" onclick="gauge6.update(NewValue());"></svg>*/}
 
     // var gauge1 = loadLiquidFillGauge("fillgauge1", 55); 
+
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on the button<span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
     var config1 = liquidFillGaugeDefaultSettings();
     config1.circleColor = "black";
     config1.textColor = "red";
